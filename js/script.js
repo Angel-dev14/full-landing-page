120e03f8af1a1bb49ce5b2012fd05058ca75eeeb
Angel-dev14 <71835930+Angel-dev14@users.noreply.github.com>
2021-02-19 23:43:16 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function (){
+
+    "use strict"
+    $("#slider-carousel").carouFredSel({
+        responsive: true,
+        width: "100%",
+        circular: true,
+        scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items:{
+            visible:{
+                min:1,
+                max:1,
+            },
+            height:"variable"
+        },
+        pagination:{
+            //container:".sliderpager",
+            pageAnchorBuilder: false
+        }
+    });
+    $("input[type=checkbox]").click(function () {
+        if ($(this).is(":checked")) {
+            console.log("click")
+            $(".nav-bar").css({
+                backgroundColor: "deepskyblue",
+                transition: "0.5s ease-in"
+            })
+        } else {
+            console.log("not checked")
+            $(".nav-bar").css({
+                backgroundColor: "transparent",
+                transition: "0.5s ease-in"
+            })
+            $(window).scroll(function () {
+
+                let top = $(window).scrollTop();
+                if (top >= 80) {
+                    $("header").addClass("secondary");
+                } else if ($("header").hasClass("secondary")) {
+                    $("header").removeClass("secondary");
+                }
+            })
+        }
+    })
+    if(window.innerWidth>767){
+        $(window).scroll(function () {
+
+            let top = $(window).scrollTop();
+            if (top >= 80) {
+                $("header").addClass("secondary");
+            } else if ($("header").hasClass("secondary")) {
+                $("header").removeClass("secondary");
+            }
+        })
+    }
+
+
+})
